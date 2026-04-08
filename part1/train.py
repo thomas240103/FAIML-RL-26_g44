fd6b7dd91e3266bc20fc7ff026f91cc69abed386
lambdavi <daviibuoso@gmail.com>
2026-04-08 16:46:16 +0200

    final commit

--- part1/train.py
+++ part1/train.py
@@ -1,6 +1,6 @@
 """Sample script for training a control policy on the Hopper environment
 
-    Here you will implement the training loop for REINFORCE and A2C
+    Here you will implement the training loop for REINFORCE and Actor-Critic
 """
 import gymnasium as gym
 
@@ -10,7 +10,7 @@ def main():
     print('State space:', env.observation_space)  # state-space
     print('Action space:', env.action_space)  # action-space
 
-    #TODO: implement training loop for REINFORCE and A2C using the agent defined in agent.py
+    #TODO: implement training loop for REINFORCE and Actor-Critic using the agent defined in agent.py
 
 if __name__ == '__main__':
     main()
